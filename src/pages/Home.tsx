decc1402cacc845760168d099ea0a4c8dfc16463
Daniel <danielfloresda802@gmail.com>
2021-07-05 17:27:22 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+
+export function Home() {
+    return(
+        <div className="page-home">
+            <aside>
+
+            </aside>
+            <main>
+                <div className="main-content">
+
+                </div>
+            </main>
+        </div>
+);
+}
+
+
